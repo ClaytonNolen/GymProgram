85670897fdf4b6d0c9bd0b2ba617f87e94cb3725
The Crow <111471385+crowleyk1@users.noreply.github.com>
2024-03-02 14:41:52 -0600

    finished event list page

---
+++ src/components/EventCard.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+
+</script>
+
+<article class="bg-secondary p-8 rounded-lg hover:bg-slate-800 hover:scale-110 duration-300 transistion-all cursor-pointer">
+    <div class="flex flex-col">
+        <h1 class="capitalize text-2xl font-bold tracking-widest">
+            eventName
+        </h1>
+        <p class="py-2 text-gray-300 font-semibold">
+            eventDescription
+        </p>
+
+        <!-- Host -->
+        <p>
+            Host
+        </p>
+        <div class="flex space-x-2 items-center">
+            <img
+                src="" 
+                alt=""
+                class="h-10 w-10 rounded-full object-cover"
+            />
+            <div class="flex flex-col">
+                <p>
+                    hostName
+                </p>
+                <p>
+                    hostEmail
+                </p>
+            </div>
+        </div>
+
+        <!-- Guest -->
+        <p class="pt-4">
+            Guest
+        </p>
+        <div class="flex space-x-2 items-center">
+            <img
+                src="" 
+                alt=""
+                class="h-10 w-10 rounded-full object-cover"
+            />
+            <div class="flex flex-col">
+                <p>
+                    hostName
+                </p>
+                <p>
+                    hostEmail
+                </p>
+            </div>
+        </div>
+
+        <p class="text-gray-400 mt-4">
+            Event Date : formattedDate
+        </p>
+        <p>
+            Members members.length
+        </p>
+    </div>
+</article>
---
+++ src/routes/main/eventlist/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import EventCard from '../../../components/EventCard.svelte'
+</script>
+
+<main class="text-gray-100 mt-10 max-w-5xl mx-auto">
+    <h1 class="text-5xl font-bold my-5">
+        Events
+    </h1>
+    <div class="grid px-4 md:grid-cols-3 gap-8">
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+    </div>
+</main>
