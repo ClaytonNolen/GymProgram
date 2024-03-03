2f208962ec8d882a767d73388357736a23784057
jedace <89285386+jedace@users.noreply.github.com>
2024-03-03 10:57:54 -0600

    Complete join page
    Code for join page which gives outline for
    creating custom equipment, gyms, and joining
    others gyms.

---
+++ src/components/EventInfo.svelte
@@ -0,0 +1,49 @@
+<script> lang="ts"</script>
+
+<section class="mt-[60px] ml-[20px] xl:ml-[277px] text-white xl:col-span-2 pr-8">
+    <p class="">formatted Date</p>
+    <h1 class="pt-2 text-3xl font-bold">name</h1>
+    <p class="pt-2">description</p>
+
+    <h2 class="pt- text-xl font-bold">Join</h2>
+
+    <form class="pt-6">
+        <div class="flex flex-col">
+            <label for="how">Where did you find us from?</label>
+            <input 
+                id="how" 
+                type="text" 
+                placeholder="Recommended by someone, sign, etc." 
+                class="py-4 pl-5 pr-24 bg-transparent border border-clr"
+            />
+        </div>
+        <div class="flex flex-col">
+            <label for="how">Are you a student or staff?</label>
+            <input 
+                id="how" 
+                type="text" 
+                placeholder="Position" 
+                class="py-4 pl-5 pr-24 bg-transparent border border-clr"
+            />
+        </div>
+        <div class="flex flex-col">
+            <label for="how">Southwestern Email</label>
+            <input 
+                id="how" 
+                type="text" 
+                placeholder="example@southwestern.edu" 
+                class="py-4 pl-5 pr-24 bg-transparent border border-clr"
+            />
+        </div>
+        <div class="flex flex-col col-span-2">
+            <label for="how">What are your goals?</label>
+            <input 
+                id="how" 
+                type="text" 
+                placeholder="I want to track my progress, increase weight, lose weight, etc." 
+                class="py-4 pl-5 pr-24 bg-transparent border border-clr"
+            />
+            <button class="py-6 px-8 bg-yellow text-black mt-8 hover:scale-105 hover:bg-green-500 transition-all duration-300 disabled:cursor-not-allowed">Submit</button>
+        </div>
+    </form>
+</section>
