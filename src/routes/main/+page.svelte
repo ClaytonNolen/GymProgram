2bd048f84f3fa82db1398bea43aa2eab6d85573e
Daniel-Haley <haleyd@southwestern.edu>
2024-03-02 21:25:39 -0600

    Updated main page to include buttons

---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts"></script>
+<main>
+        <!--Main Page-->
+    <div class="relative mt-[150px] p-2 lg:ml-[133px] flex"> 
+        <div> 
+            <h1 class="text-[32px] font-bold text-white lg:w-[710px]"> 
+                Welcome to The Gym Program
+            </h1>
+            <div class="flex flex-col px-10 sm:flex-row mt-20 gap-14">
+                <button class="py-[23px] px-[86px] bg-black text-xl text-white w-[299px] hover:bg-white hover:text-black duration-300 transittion-colors">
+                    Start a Rob Workout</button>
+                <button class="py-[23px] px-[86px] bg-black text-xl text-white w-[299px] hover:bg-white hover:text-black duration-300 transittion-colors">
+                    Track Your Progress</button>
+            </div>
+        </div>
+    </div>
+</main>
