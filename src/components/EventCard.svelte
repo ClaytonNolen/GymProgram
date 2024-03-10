7729d443b4e0c91da84579af53e00851d719ab2b
ClaytonNolen <nolenc@southwestern.edu>
2024-03-10 18:15:02 -0500

    eventList page binded, but need other files to double check.

--- src/components/EventCard.svelte
+++ src/components/EventCard.svelte
@@ -1,61 +1,62 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+    import { formatDate } from "../helper/formatDate";
 
+    import type { Member } from '../types';
+
+    export let eventName: string;
+    export let eventDescription: string; 
+    export let eventDate: string; 
+    export let guestName: string; 
+    export let guestPhoto: string; 
+    export let guestDesignation: string; 
+    export let hostName: string; 
+    export let hostPhoto: string; 
+    export let hostemail: string; 
+    export let members: Member[];
+
+    const formattedDate = formatDate(eventDate);
 </script>
 
-<article class="bg-secondary p-8 rounded-lg hover:bg-slate-800 hover:scale-110 duration-300 transistion-all cursor-pointer">
-    <div class="flex flex-col">
-        <h1 class="capitalize text-2xl font-bold tracking-widest">
-            eventName
-        </h1>
-        <p class="py-2 text-gray-300 font-semibold">
-            eventDescription
-        </p>
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- If the click does not work check the pt2 at 44min. -->
+<article 
+on:click={() => goto(`/join/$(eventName)`)}
+on:keydown={() => {}}
+    class="bg-secondary p-8 rounded-lg hover:bg-slate-800 hover:scale-110 duration-300 transistion-all cursor-pointer">
+        <div class="flex flex-col">
+            <h1 class="capitalize text-2xl font-bold tracking-widest">{eventName}</h1>
+            <p class="py-2 text-gray-300 font-semibold">{eventDescription}</p>
 
-        <!-- Host -->
-        <p>
-            Host
-        </p>
-        <div class="flex space-x-2 items-center">
-            <img
-                src="" 
-                alt=""
-                class="h-10 w-10 rounded-full object-cover"
-            />
-            <div class="flex flex-col">
-                <p>
-                    hostName
-                </p>
-                <p>
-                    hostEmail
-                </p>
+            <!-- Host -->
+            <p>Host</p>
+            <div class="flex space-x-2 items-center">
+                <img
+                    src={hostPhoto} 
+                    alt={hostName}
+                    class="h-10 w-10 rounded-full object-cover"
+                />
+                <div class="flex flex-col">
+                    <p>{hostName}</p>
+                    <p>{hostemail}</p>
+                </div>
             </div>
-        </div>
 
-        <!-- Guest -->
-        <p class="pt-4">
-            Guest
-        </p>
-        <div class="flex space-x-2 items-center">
-            <img
-                src="" 
-                alt=""
-                class="h-10 w-10 rounded-full object-cover"
-            />
-            <div class="flex flex-col">
-                <p>
-                    hostName
-                </p>
-                <p>
-                    hostEmail
-                </p>
+            <!-- Guest -->
+            <p class="pt-4">Guest</p>
+            <div class="flex space-x-2 items-center">
+                <img
+                    src={guestPhoto} 
+                    alt="hostName"
+                    class="h-10 w-10 rounded-full object-cover"
+                />
+                <div class="flex flex-col">
+                    <p>{guestName}</p>
+                    <p>{guestDesignation}</p>
+                </div>
             </div>
-        </div>
 
-        <p class="text-gray-400 mt-4">
-            Event Date : formattedDate
-        </p>
-        <p>
-            Members members.length
-        </p>
-    </div>
+            <p class="text-gray-400 mt-4">Event Date : {formattedDate}</p>
+            <p>Members {members.length}</p>
+        </div>
 </article>
---
+++ src/routes/join/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import EventInfo from "../../../components/EventInfo.svelte";
+	import GuestInfo from "../../../components/GuestInfo.svelte";
+
+</script>
+
+<div class="grid xl:grid-cols-3">
+    <EventInfo/>
+    <GuestInfo/>
+</div>
