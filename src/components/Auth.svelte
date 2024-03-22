790df6a90508d3a86a71bda3e691e1057980359e
ClaytonNolen <nolenc@southwestern.edu>
2024-03-22 10:28:56 -0500

    Login Buf Fixed

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,15 +1,25 @@
 <!-- For Button ICON: https://fontawesome.com/icons -->
 
-<script language="ts">
-    import { authHandlers } from "$lib/assets/gym/gym";
+<script lang="ts">
+    import type { User } from 'firebase/auth';
+    import { authStore, authHandlers } from '$lib/assets/gym/gym';
+
+    let currentUser : User | null
+    authStore.subscribe((value) => {
+        currentUser = value.user
+    })
 </script>
 
-<main class="flex flex-col items-center justify-center">
-    <div class="mt-12">
-        <h1 class="text-2xl text-white font-bold">Login</h1>
-        <button on:click = { authHandlers.loginWithGoogle } class="p-4 bg-white rounded-lg mt-12 font-bold">
-            <i class="fa-brands fa-google fa-xl" style="color: #FFD43B;"></i>
-            Sign In with Google
-        </button>
-    <div/>
-</main>
+{#if currentUser}
+<h1 class="text-2xl text-white font-bold">CURRENT USER IS LOGGED IN</h1>
+{:else}
+    <main class="flex flex-col items-center justify-center">
+        <div class="mt-12">
+            <h1 class="text-2xl text-white font-bold">Login</h1>
+            <button on:click = { authHandlers.loginWithGoogle } class="p-4 bg-white rounded-lg mt-12 font-bold">
+                <i class="fa-brands fa-google fa-xl" style="color: #FFD43B;"></i>
+                Sign In with Google
+            </button>
+        <div/>
+    </main>
+{/if}
