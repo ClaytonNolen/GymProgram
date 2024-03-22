cca1cfb18d339a88ddf6991d522499765f35e380
The Crow <111471385+crowleyk1@users.noreply.github.com>
2024-03-22 10:45:09 -0500

    blah
    blah blah

---
+++ src/routes/SUInfo/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<h1 class="mt-4 text-center text-white text-3xl">
+    The Robertson Gym Equipment
+</h1>
+
+<div class="border-double border-4 border-white-500">
+    <div>
+        <h1>
+            Ab Crunch
+        </h1>
+        
+    </div>
+</div>
