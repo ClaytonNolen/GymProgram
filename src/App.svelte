53e28b27574d030c4d0abf312d6e97942989b0f5
Daniel-Haley <haleyd@southwestern.edu>
2024-03-24 20:02:01 -0500

    Added Support for Chart.js
    Still needs testing and link to Firebase

---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<script>
+    // import Chart from './Chart.Svelte';
+    // Video: https://www.youtube.com/watch?v=s7rk2b1ioVE explains how data can be added to a secific chart in 'Chart.svelte'
+</script>
