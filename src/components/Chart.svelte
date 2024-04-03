22e8e163a5bcb33d7cacf7cab9ae81443cb3451e
Daniel-Haley <haleyd@southwestern.edu>
2024-04-03 10:41:24 -0500

    Chart Almost Working
    Need to figure out an error with how the data is structured but close to being able to see an example chart.

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,1 +1,28 @@
-<!---->
+<script>
+    import { Line } from 'svelte-chartjs';
+    import 'chart.js/auto';
+    import { data } from './GymData.js';
+  
+    import {
+      Chart as ChartJS,
+      Title,
+      Tooltip,
+      Legend,
+      LineElement,
+      LinearScale,
+      PointElement,
+      CategoryScale,
+    } from 'chart.js';
+  
+    ChartJS.register(
+      Title,
+      Tooltip,
+      Legend,
+      LineElement,
+      LinearScale,
+      PointElement,
+      CategoryScale
+    );
+  </script>
+  
+  <Line data={data} width={100} height={50} options={{ maintainAspectRatio: false}} />
