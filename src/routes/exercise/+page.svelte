1082fdce2657d8cc0dacf4f2bda7f1921ccd1678
The Crow <111471385+crowleyk1@users.noreply.github.com>
2024-04-05 10:39:08 -0500

    akldfjlasdjf

---
+++ src/routes/exercise/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<main>
+    <input
+        id="name"
+        type="text"
+        placeholder="Name"
+    />
+    <input
+        id="sets"
+        type="text"
+        placeholder="Sets"
+    />
+    <input
+        id="reps" 
+        type="text" 
+        placeholder="Reps" 
+    />
+    <input
+        id="weight" 
+        type="text" 
+        placeholder="Weight" 
+    />
+</main>
