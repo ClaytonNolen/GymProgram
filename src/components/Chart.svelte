e1f9c8f9926a0a667949a9ae4e167a53c2918181
Daniel-Haley <haleyd@southwestern.edu>
2024-04-06 17:17:58 -0500

    Fixed Chart
    Deleted data.js and App.svelte. Centered the chart but still need to figure out sizing.

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -1,70 +1,45 @@
- <!-- <script>
-    import { Line } from 'svelte-chartjs';
-    import 'chart.js/auto';
-    import { data } from './data.js';
+<!--Code partly from: https://www.chartjs.org/docs/latest/getting-started/ and adapted with https://www.youtube.com/watch?v=JvYK-X2Tf8E
+to implement onMount-->
+<!--Link for HTML Canvas: https://www.w3schools.com/html/html5_canvas.asp-->
+
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import Chart from 'chart.js/auto';
   
-    import {
-      Chart as ChartJS,
-      Title,
-      Tooltip,
-      Legend,
-      LineElement,
-      LinearScale,
-      PointElement,
-      CategoryScale,
-    } from 'chart.js';
-  
-    ChartJS.register(
-      Title,
-      Tooltip,
-      Legend,
-      LineElement,
-      LinearScale,
-      PointElement,
-      CategoryScale
-    );
-  </script>
-  -->
-  <!--<Line data={data} options={{ responsive: true}} />-->
+  let chartData = [12, 19, 3, 5, 2, 3]; // Possibly where Firebase data
+  let labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+  let canvas: HTMLCanvasElement;
 
-  <script lang="ts">
-    import {onMount} from 'svelte';
-    import Chart from 'chart.js/auto';
-    
-    let chartData = [12, 19, 3, 5, 2, 3];
-    let labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-    let canvas: HTMLCanvasElement;
+  onMount(async ()=> {
+      const ctx = canvas.getContext('2d');
+      // Line below was given by AI to fix an error with ctx possibly having a null value
+      if (!ctx) return;
 
-    onMount(async ()=> {
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
+      new Chart(ctx, { // Need to edit later for styling
+      type: 'line',
+      data: {
+          labels: labels,
+          datasets: [{
+          label: '# of Votes',
+          data: chartData,
+          backgroundColor: 'rgba(255,159,64,1)',
+          borderColor: 'rgba(255,159,64,1)',
+          borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+          y: {
+              beginAtZero: true
+          }
+          }
+      }
+  });
+      }
+  );
 
-        new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-            label: '# of Votes',
-            data: chartData,
-            backgroundColor: 'rgba(255,159,64,1)',
-            borderColor: 'rgba(255,159,64,1)',
-            borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-            y: {
-                beginAtZero: true
-            }
-            }
-        }
-    });
-        }
-    );
-
-  </script>
+</script>
 
 <div>
-    <canvas id="myChart" width = "100" height="100" bind:this={canvas}></canvas>
+  <canvas id="myChart" style = "width = 500x; height= 500px;" bind:this={canvas}></canvas>
 </div>
-  
