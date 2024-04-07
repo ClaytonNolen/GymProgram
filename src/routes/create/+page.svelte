5a40ff533d3664ffb9fd53d15ad63769a3274b1a
jedace <89285386+jedace@users.noreply.github.com>
2024-04-06 21:47:28 -0500

    Workouts & Exercises
    workout page exists and can create a workout
    card. there is no ability to add multiple
    exercises to a workout though due to
    variable binding issues

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,165 +1,288 @@
-<script lang="ts">
-    import { doc, setDoc } from 'firebase/firestore';
-    import { db, storage } from '$lib/firebase';
-    import type { User } from 'firebase/auth';
-    import { authStore } from '$lib/assets/gym/gym';
-    import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-    import { goto } from '$app/navigation';
-    import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
+    <script lang="ts">
+        import { doc, setDoc } from 'firebase/firestore';
+        import { db, storage } from '$lib/firebase';
+        import type { User } from 'firebase/auth';
+        import { authStore } from '$lib/assets/gym/gym';
+        import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+        import { goto } from '$app/navigation';
+        import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
 
 
-    let workout: string;
-    let workoutNotes: string;
-    let workoutDate: string;
-    let workoutTitle: string;
-    // let answer: string;
+        let workout: string = "Select Workout";
+        let workoutNotes: string;
+        let workoutDate: string;
+        let workoutTitle: string;
+        let sets: number | null = null;
+        let reps: number | null = null;
+        let weight: number | null = null;
+        let showExerciseInfo: boolean = false;
+        // let answer: string;
+        let answer = '';
+        let selectedExercise: {id: number, text: string} | null = null
+        let selectedExerciseArr: { 
+            id: number,
+            workout:string, 
+            exercise:string,
+            sets:number | null,
+            reps:number | null,
+            weight:number | null}[] =[];
+     
 
-    let gymName: string;
-    let gymDescription: string;
-    let gymDate: string;
-    let maxCapacity: number;
-    let gymPhoto: File;
-    let availableEquipment: string;
-    let loading = false;
-    let currentUser: User | null = null;
-    authStore.subscribe((value) => {
-        currentUser = value.user;
-    });
+        let exercises = [
+            {id: 0, text:'Select Workout:'},
+            {id: 1, text:'Ab Bench'},
+            {id: 2, text:'Abdominal Crunch'},
+            {id: 3, text:'Back Extension'},
+            {id: 4, text:'Bench Press'},
+            {id: 5, text:'Bicep Curl'},
+            {id: 6, text:'Cable Machine'},
+            {id: 7, text:'Calf Raise'},
+            {id: 8, text:'Chest Press'},
+            {id: 9, text:'Glute Machine'},
+            {id: 10, text:'Hip Adductor'},
+            {id: 11, text:'Lat Pull Down'},
+            {id: 12, text:'Lateral Raise'},
+            {id: 13, text:'Leg Extension'},
+            {id: 14, text:'Leg Press'},
+            {id: 15, text:'Pec Fly'},
+            {id: 16, text:'Plated Chest Press'},
+            {id: 17, text:'Plated Leg Press'},
+            {id: 18, text:'Rotary Torso'},
+            {id: 19, text:'Seated Leg Curl'},
+            {id: 20, text:'Seated Row'},
+            {id: 21, text:'Shoulder Press'},
+            {id: 22, text:'Tricep Press'},
+        ];
+        let loading = false;
+        let currentUser: User | null = null;
+        authStore.subscribe((value) => {
+            currentUser = value.user;
+        });
 
-    function handleFileInputChange(event: Event) {
-		const inputElement = event.target as HTMLInputElement;
-		if (inputElement.files && inputElement.files.length > 0) {
-			gymPhoto = inputElement.files[0];
-		}
-	}
+        function handleWorkoutSelection() {
+            if (workout !== 'Select Workout:'){
+                showExerciseInfo = true;
+                selectedExercise = exercises.find(exercise => exercise.text === workout) || null;
+            } else {
+                showExerciseInfo = false;
+                selectedExercise = null;
+            }
+        }
 
-    async function createGym() {
-		if (workout === undefined || loading === true)
-			return alert('Must include at least one workout');
-		loading = true;
-    
+        // function addMultipleExercises() {
+        //     if (selectedExercise && sets !== null && reps !== null && weight !== null) {
+        //         selectedExerciseArr.push({
+        //             id: id,
+        //             workout: workout,
+        //             exercise: selectedExercise.text,
+        //             sets: sets,
+        //             reps: reps,
+        //             weight: weight
+        //         });
+        //     }
+        // }
 
-        const workoutInfo = {
-            workout: workout,
-            workoutNotes: workoutNotes,
-            workoutDate: workoutDate,
-            workoutTitle: workoutTitle,
-            answer: answer
-        };
+        function addMultipleExercises() {
+            if (selectedExercise && sets !== null && reps !== null && weight !== null) {
+                const id = selectedExerciseArr.length;
+                const exerciseObj = {
+                    id: id,
+                    workout: workout,
+                    exercise: selectedExercise.text,
+                    sets: sets,
+                    reps: reps,
+                    weight: weight
+                }
+                selectedExerciseArr.push(exerciseObj);
+                    // sets = null;
+                    // reps = null;
+                    // weight = null;
+            }
+        }
 
-        try {
-			const gymRef = doc(db, 'workouts', workout);
-			setDoc(gymRef, workoutInfo, { merge: true });
-			goto('/profile');
-		} catch (error) {
-            console.log(answer)
+        async function createGym() {
+            if (workout === undefined || loading === true)
+                return alert('Must include at least one workout');
+            loading = true;
+        
+            const workoutInfo = {
+                workout: workout,
+                workoutNotes: workoutNotes,
+                workoutDate: workoutDate,
+                workoutTitle: workoutTitle,
+                answer: answer,
+                sets: sets,
+                reps: reps,
+                weight: weight,
+                showExerciseInfo: showExerciseInfo
 
-			console.log(`An error ocuured while creating a document ${error}`);
-		}
-		loading = false;
-    }
+            };
 
-    let exercises = [
-        {id: 0, text:'Select Workout:'},
-        {id: 1, text:'Ab Bench'},
-        {id: 2, text:'Abdominal Crunch'},
-        {id: 3, text:'Back Extension'},
-        {id: 4, text:'Bench Press'},
-        {id: 5, text:'Bicep Curl'},
-        {id: 6, text:'Cable Machine'},
-        {id: 7, text:'Calf Raise'},
-        {id: 8, text:'Chest Press'},
-        {id: 9, text:'Glute Machine'},
-        {id: 10, text:'Hip Adductor'},
-        {id: 11, text:'Lat Pull Down'},
-        {id: 12, text:'Lateral Raise'},
-        {id: 13, text:'Leg Extension'},
-        {id: 14, text:'Leg Press'},
-        {id: 15, text:'Pec Fly'},
-        {id: 16, text:'Plated Chest Press'},
-        {id: 17, text:'Plated Leg Press'},
-        {id: 18, text:'Rotary Torso'},
-        {id: 19, text:'Seated Leg Curl'},
-        {id: 20, text:'Seated Row'},
-        {id: 21, text:'Shoulder Press'},
-        {id: 22, text:'Tricep Press'},
-    ];
+            try {
+                const gymRef = doc(db, 'workouts', workoutTitle);
+                setDoc(gymRef, workoutInfo, { merge: true });
+                goto('/profile');
+            } catch (error) {
+                console.log(answer)
 
-    // let selected: typeof exercises[];
+                console.log(`An error ocuured while creating a document ${error}`);
+            }
+            loading = false;
+        }
 
-    let answer = '';
+    </script>
 
-    // function handleSubmit() {
-    //     alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
-    // }    
-</script>
+    <main class="text-gray-100 mt-10">
+        <div>
+            <!-- input box -->
+            <div class="max-w-4xl mx-auto bg-secondary rounded-lg flex flex-col p-5">
+                <h1 class="text-center text-white text-2xl">Create</h1>
 
-<main class="text-gray-100 mt-10">
-    <div>
-        <!-- input box -->
-        <div class="max-w-4xl mx-auto bg-secondary rounded-lg flex flex-col p-5">
-            <h1 class="text-center text-white text-2xl">Create</h1>
-
-             <!-- Workout Name-->
-             <div class="flex flex-col my-4">
-                <label for="workout-title">Workout Name</label>
-                <input 
-                    id="workout-title" 
-                    type="text" 
-                    bind:value={workoutTitle}
-                    placeholder="Workout Name e.g. Leg Day"
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
-
-            <!-- Workout -->
-            <div class="flex flex-col my-4">
-                <label for="workout">Exercise</label>
-                <select bind:value={workout} on:change={() => (answer = '')}
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                    id = "workout"
-                    >
-                    {#each exercises as exercise}
-                    <option value={exercise}>
-                        {exercise.text}</option>
-                    {/each}
+                <!-- Workout Name-->
+                <div class="flex flex-col my-4">
+                    <label for="workout-title">Workout Name</label>
                     <input 
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                    bind:value={answer} />
-                </select>
-                    
+                        id="workout-title" 
+                        type="text" 
+                        bind:value={workoutTitle}
+                        placeholder="Workout Name e.g. Leg Day"
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                    />
+                </div>
+    
+                <!-- Workout -->
+                <style>
+                    select {
+                        background-color: #2b2d38;
+                    }
+                    select option {
+                        color: #000000;
+                    }
+                </style>
+                <div class="flex flex-col my-4">
+                    <label for="workout">Exercise</label>
+                    <select bind:value={workout} on:change={handleWorkoutSelection}
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                        id = "workout"
+                        >
+                        {#each exercises as exercise}
+                        <option value={exercise.text}>  
+                            {exercise.text}
+                        </option>
+                        {/each}
+                        <input 
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                        />
+                    </select>
+                </div>
+
+                <!-->Exercise sets, reps, weight-->
+                {#if showExerciseInfo}
+                <div class="flex flex-row my-4">
+                    <!--Sets-->
+                        <div class="flex flex-col">
+                            <label for="sets">Sets</label>
+                            <input 
+                                id="sets"
+                                type="number"
+                                bind:value={sets}
+                                placeholder="Sets"
+                                class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                            />
+                        </div>
+                    <!--Reps-->
+                        <div class="flex flex-col">
+                            <label for="reps">Reps</label>
+                            <input 
+                                id="reps"
+                                type="number"
+                                bind:value={reps}
+                                placeholder="Reps"
+                                class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                            />
+                        </div>
+                        <!--Reps-->
+                        <div class="flex flex-col">
+                            <label for="weight">Weight</label>
+                            <input 
+                                id="weight"
+                                type="number"
+                                bind:value={weight}
+                                placeholder="Weight"
+                                class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                            />
+                        </div>
+                    </div>
+                {/if}
+
                 
-            </div>
+                <!-- Workout Date -->
+                <div class="flex flex-col my-4">
+                    <label for="workout-date">Workout Date</label>
+                    <input  
+                        id="workout-date" 
+                        type="date" 
+                        bind:value={workoutDate}
+                        placeholder="Enter Date"
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                    />
+                </div>
 
-             <!-- Workout Date -->
-             <div class="flex flex-col my-4">
-                <label for="workout-date">Workout Date</label>
-                <input 
-                    id="workout-date" 
-                    type="date" 
-                    bind:value={workoutDate}
-                    placeholder="Enter Date"
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
+                <!-- Workout Notes -->
+                <style>
+                    textarea {
+                        width: 100%;
+                        resize: vertical;
+                        height: 200px;
+                    }
+                </style>
+                <div class="flex flex-col my-4">
+                    <label for="workout-notes">Notes</label>
+                    <textarea 
+                        id="workout-notes" 
+                        bind:value={workoutNotes}
+                        placeholder="Additional Workout Information...
+    100 Pushups
+    100 Sit Ups
+    100 Squats
+    10 km Run"
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                    ></textarea>
+                </div>
 
-            <!-- Workout Notes -->
-            <div class="flex flex-col my-4">
-                <label for="workout-notes">Notes</label>
-                <input 
-                    id="workout-notes" 
-                    type="text" 
-                    bind:value={workoutNotes}
-                    placeholder="Additional Workout Information..."
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
+                <button 
+                disabled={loading}
+                on:click={addMultipleExercises}
+                class="py-2 px-8 bg-white text-black mt-8 disabled:bg-white/25 disabled:cursor-not-allowed"
+                >{loading ? 'Creating' : 'Add Exercise'}</button>
+                {#each selectedExerciseArr as exercise}
+                <div class="flex flex-col my-4">
+                    <label for="exercise-{exercise.id}">Exercise</label>
+                    <input 
+                        id="exercise-{exercise.id}" 
+                        type="text" 
+                        value={exercise.exercise}
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                        readonly
+                    />
+                    <!-- Add input fields for sets, reps, and weight-->
+                    <label for="sets-{exercise.id}">Sets</label>
+                    <input 
+                        id="sets-{exercise.id}" 
+                        type="number" 
+                        value={exercise.sets}
+                        class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
+                        readonly
+                    />
+                    <!-- reps and weight -->
+                </div>
+            {/each}
+                <button 
+                disabled={loading}
+                on:click={createGym}
+                class="py-2 px-8 bg-white text-black mt-8 disabled:bg-white/25 disabled:cursor-not-allowed"
+                >{loading ? 'Creating' : 'Create Workout'}</button>
 
-            <button 
-            disabled={loading}
-            on:click={createGym}
-            class="py-2 px-8 bg-white text-black mt-8 disabled:bg-white/25 disabled:cursor-not-allowed"
-            >{loading ? 'Creating' : 'Create'}</button>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
