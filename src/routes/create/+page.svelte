b08c6dec6ae049af151f07ebc8795a876dad6bff
The Crow <111471385+crowleyk1@users.noreply.github.com>
2024-04-13 15:11:29 -0500

    test

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,109 +1,33 @@
-<script lang="ts">
-    import { doc, setDoc } from 'firebase/firestore';
-    import { db, storage } from '$lib/firebase';
-    import type { User } from 'firebase/auth';
-    import { authStore } from '$lib/assets/gym/gym';
-    import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-    import { goto } from '$app/navigation';
-    import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
-    
-    let workoutName: String
-    let exerciseName: String
-    let sets: number | null
-    let reps: number | null
-    let weight: number | null
-    let exercise: any[] = []
-    let exercises: any[] = []
+<script>
+	import { Object, Array, bind } from 'svelte-object'
 
-    async function addExercise() {
-        exercise.push(exerciseName);
-        exercise.push(sets);
-        exercise.push(reps);
-        exercise.push(weight);
-        exercises.push(exercise);
-        exercise.pop
-        exercise.pop
-        exercise.pop
-        exercise.pop
-        exerciseName=""
-        sets=null
-        reps=null
-        weight=null
-        return alert(exercises[0])
-    }
-
-    function createWorkout() {
-        const workoutInfo = {
-                workoutName: workoutName,
-                exercises: exercises
-            };
-
-            try {
-                const gymRef = doc(db, 'workoutTest', workoutName);
-                setDoc(gymRef, workoutInfo, { merge: true });
-                goto('/profile');
-            } catch (error) {
-                console.log(`An error ocuured while creating a document ${error}`);
-            }
-    }
+  let data = [{
+		name: 'Lillemis',
+		age: 5,
+		editable: 'You can edit this text'
+	}]
+	
+	let store
 </script>
 
-<main>
-    <h1 class="text-center text-white">
-        New Workout
-    </h1>
-    <div class=" bg-secondary rounded-lg mx-auto">
-        <input
-            class="my-2"
-            bind:value={workoutName}
-            placeholder="Workout Name"
-        />
-        <h2 class="text-white">
-            Exercises
-        </h2>
-        <!--Should display array but svelte is stupid-->
-        {#each exercises as ex}
-            {ex}
-        {/each}
-        <input
-            class="my-2"
-            bind:value={exerciseName}
-            placeholder="Exercise Name"
-        />
-        <input
-            class="my-2"
-            bind:value={sets}
-            placeholder="Sets"
-        />
-        <input
-            class="my-2"
-            bind:value={reps}
-            placeholder="Reps"
-        />
-        <input
-            class="my-2"
-            bind:value={weight}
-            placeholder="Weight"
-        />
-        <button
-            class="bg-black text-white rounded-lg p-1"
-            on:click={addExercise}>
-            Add Exercise
-        </button>
-        <button
-            class="bg-black text-white rounded-lg p-1"
-            on:click={createWorkout}>
-            Create Workout
-        </button>
-    </div>
-</main>
 
-<style>
-    main {
-        text-align: center;
-    }
-
-    div {
-        width: 50%;
-    }
-</style>
+<div>
+	<Object bind:store let:store let:value value={{ name: 'Arthurs Pet Company' }}>
+		<h2>{value.name}</h2>
+		<input use:store={'name'}>
+		<Array name='pets' let:value value={data}>
+			<h3>Pets</h3>
+			{#each value as item, k}
+				<div>
+					<Object let:store name={k}>
+						<input use:store={'name'} />
+						<input use:store={'age'} type='number' />
+						<div use:store={'editable'}> {item.editable || ''} </div>
+					</Object>
+					<button on:click={() => value.removeByIndex(k)}>- {item.name || ''}</button>
+				</div>
+			{/each}
+			<button on:click={() => value.push({})}>+</button>
+		</Array>
+	</Object>
+</div>
