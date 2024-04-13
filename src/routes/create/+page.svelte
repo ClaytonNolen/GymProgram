f3f0a3aebf1347a0b8439b8d86a8feef23868a7d
The Crow <111471385+crowleyk1@users.noreply.github.com>
2024-04-13 14:48:44 -0500

    test

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -6,108 +6,93 @@
     import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
     import { goto } from '$app/navigation';
     import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
-
-
-    let workout: string;
-    let workoutNotes: string;
-    let workoutDate: string;
-    let workoutTitle: string;
-
-    let gymName: string;
-    let gymDescription: string;
-    let gymDate: string;
-    let maxCapacity: number;
-    let gymPhoto: File;
-    let availableEquipment: string;
-    let loading = false;
-    let currentUser: User | null = null;
-    authStore.subscribe((value) => {
-        currentUser = value.user;
-    });
-
-    async function createGym() {
-		if (workout === undefined || loading === true)
-			return alert('Must include at least one workout');
-		loading = true;
     
-        const workoutInfo = {
-            workout: workout,
-            workoutNotes: workoutNotes,
-            workoutDate: workoutDate,
-            workoutTitle: workoutTitle
-        };
+    let workoutName: String
+    let exerciseName: String
+    let sets: number | null
+    let reps: number | null
+    let weight: number | null
+    let exercise: any[] = []
+    let exercises: any[] = []
 
-        try {
-			const gymRef = doc(db, 'workouts', workout);
-			setDoc(gymRef, workoutInfo, { merge: true });
-			goto('/profile');
-		} catch (error) {
-			console.log(`An error ocuured while createing a document ${error}`);
-		}
-		loading = false;
+    async function addExercise() {
+        exercise.push(exerciseName);
+        exercise.push(sets);
+        exercise.push(reps);
+        exercise.push(weight);
+        exercises.push(exercise);
+        exercise.pop
+        exercise.pop
+        exercise.pop
+        exercise.pop
+        exerciseName=""
+        sets=null
+        reps=null
+        weight=null
+        return alert(exercises[0])
     }
-    
+
+    function createWorkout() {
+        
+    }
 </script>
 
-<main class="text-gray-100 mt-10">
-    <div>
-        <!-- input box -->
-        <div class="max-w-4xl mx-auto bg-secondary rounded-lg flex flex-col p-5">
-            <h1 class="text-center text-white text-2xl">Create</h1>
-
-             <!-- Workout Name-->
-             <div class="flex flex-col my-4">
-                <label for="workout-title">Workout Name</label>
-                <input 
-                    id="workout-title" 
-                    type="text" 
-                    bind:value={workoutTitle}
-                    placeholder="Workout Name e.g. Leg Day"
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
-
-            <!-- Workout -->
-            <div class="flex flex-col my-4">
-                <label for="workout">Exercise</label>
-                <input 
-                    id="workout" 
-                    type="string" 
-                    bind:value={workout}
-                    placeholder= "Select Workout..."
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
-
-             <!-- Workout Date -->
-             <div class="flex flex-col my-4">
-                <label for="workout-date">Workout Date</label>
-                <input 
-                    id="workout-date" 
-                    type="date" 
-                    bind:value={workoutDate}
-                    placeholder="Enter Date"
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
-
-            <!-- Workout Notes -->
-            <div class="flex flex-col my-4">
-                <label for="workout-notes">Notes</label>
-                <input 
-                    id="workout-notes" 
-                    type="text" 
-                    bind:value={workoutNotes}
-                    placeholder="Additional Workout Information..."
-                    class="py-4 pl-5 pr-24 bg-24 bg-transparent border border-borderclr"
-                />
-            </div>
-
-            <button 
-            disabled={loading}
-            on:click={createGym}
-            class="py-2 px-8 bg-white text-black mt-8 disabled:bg-white/25 disabled:cursor-not-allowed"
-            >{loading ? 'Creating' : 'Create'}</button>
-        </div>
+<main>
+    <h1 class="text-center text-white">
+        New Workout
+    </h1>
+    <div class=" bg-secondary rounded-lg mx-auto">
+        <input
+            class="my-2"
+            bind:value={workoutName}
+            placeholder="Workout Name"
+        />
+        <h2 class="text-white">
+            Exercises
+        </h2>
+        <!--Should display array but svelte is stupid-->
+        {#each exercises as ex}
+            {ex}
+        {/each}
+        <input
+            class="my-2"
+            bind:value={exerciseName}
+            placeholder="Exercise Name"
+        />
+        <input
+            class="my-2"
+            bind:value={sets}
+            placeholder="Sets"
+        />
+        <input
+            class="my-2"
+            bind:value={reps}
+            placeholder="Reps"
+        />
+        <input
+            class="my-2"
+            bind:value={weight}
+            placeholder="Weight"
+        />
+        <button
+            class="bg-black text-white rounded-lg p-1"
+            on:click={addExercise}>
+            Add Exercise
+        </button>
+        <button
+            class="bg-black text-white rounded-lg p-1"
+            on:click={createWorkout}>
+            Create Workout
+        </button>
     </div>
 </main>
+
+<style>
+    main {
+        text-align: center;
+    }
+
+    div {
+        width: 50%;
+    }
+</style>
